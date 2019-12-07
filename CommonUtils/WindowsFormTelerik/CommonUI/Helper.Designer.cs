081322bf7b9d88219b0aeef42ee99c8f0ac2d618
[·Æï]-ÌÆС¶« <13594525237@139.com>
2019-12-07 11:33:04 +0800

    update

--- CommonUtils/WindowsFormTelerik/CommonUI/Helper.Designer.cs
+++ CommonUtils/WindowsFormTelerik/CommonUI/Helper.Designer.cs
@@ -29,82 +29,156 @@
         private void InitializeComponent()
         {
             this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.radButton1 = new Telerik.WinControls.UI.RadButton();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.materialTheme1 = new Telerik.WinControls.Themes.MaterialTheme();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel3 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel4 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel5 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel6 = new Telerik.WinControls.UI.RadLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radLabel7 = new Telerik.WinControls.UI.RadLabel();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel7)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(189, 58);
+            this.label3.Location = new System.Drawing.Point(119, 97);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.Size = new System.Drawing.Size(100, 17);
             this.label3.TabIndex = 9;
             this.label3.Text = "021-58921883";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(118, 58);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "技术支持：";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(118, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(72, 13);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "官方网站：";
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(189, 26);
+            this.linkLabel1.Location = new System.Drawing.Point(119, 70);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(92, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(111, 17);
             this.linkLabel1.TabIndex = 6;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "www.figkey.com";
             // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(122, 157);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(66, 24);
+            this.radButton1.TabIndex = 11;
+            this.radButton1.Text = "确定(O)";
+            this.radButton1.ThemeName = "Breeze";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Location = new System.Drawing.Point(58, 39);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(55, 21);
+            this.radLabel2.TabIndex = 13;
+            this.radLabel2.Text = "版本：";
+            this.radLabel2.ThemeName = "Material";
+            // 
+            // radLabel3
+            // 
+            this.radLabel3.Location = new System.Drawing.Point(122, 42);
+            this.radLabel3.Name = "radLabel3";
+            this.radLabel3.Size = new System.Drawing.Size(29, 18);
+            this.radLabel3.TabIndex = 14;
+            this.radLabel3.Text = "1.0.0";
+            // 
+            // radLabel4
+            // 
+            this.radLabel4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel4.Location = new System.Drawing.Point(12, 198);
+            this.radLabel4.Name = "radLabel4";
+            this.radLabel4.Size = new System.Drawing.Size(290, 18);
+            this.radLabel4.TabIndex = 15;
+            this.radLabel4.Text = "版权所有 （C）2019，丰柯电子科技（上海）有限公司";
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Location = new System.Drawing.Point(58, 12);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(52, 21);
+            this.radLabel1.TabIndex = 16;
+            this.radLabel1.Text = "STENT";
+            this.radLabel1.ThemeName = "Material";
+            // 
+            // radLabel5
+            // 
+            this.radLabel5.Location = new System.Drawing.Point(27, 66);
+            this.radLabel5.Name = "radLabel5";
+            this.radLabel5.Size = new System.Drawing.Size(86, 21);
+            this.radLabel5.TabIndex = 17;
+            this.radLabel5.Text = "官方网站：";
+            this.radLabel5.ThemeName = "Material";
+            // 
+            // radLabel6
+            // 
+            this.radLabel6.Location = new System.Drawing.Point(27, 93);
+            this.radLabel6.Name = "radLabel6";
+            this.radLabel6.Size = new System.Drawing.Size(86, 21);
+            this.radLabel6.TabIndex = 18;
+            this.radLabel6.Text = "技术支持：";
+            this.radLabel6.ThemeName = "Material";
+            // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::WindowsFormTelerik.Properties.Resources.figkeyLogo;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Image = global::WindowsFormTelerik.Properties.Resources.fg;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 44);
+            this.pictureBox1.Size = new System.Drawing.Size(34, 31);
             this.pictureBox1.TabIndex = 10;
             this.pictureBox1.TabStop = false;
             // 
-            // radButton1
+            // radLabel7
             // 
-            this.radButton1.Location = new System.Drawing.Point(220, 98);
-            this.radButton1.Name = "radButton1";
-            this.radButton1.Size = new System.Drawing.Size(61, 24);
-            this.radButton1.TabIndex = 11;
-            this.radButton1.Text = "确定(O)";
-            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            this.radLabel7.Location = new System.Drawing.Point(27, 120);
+            this.radLabel7.Name = "radLabel7";
+            this.radLabel7.Size = new System.Drawing.Size(86, 21);
+            this.radLabel7.TabIndex = 20;
+            this.radLabel7.Text = "电子邮箱：";
+            this.radLabel7.ThemeName = "Material";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 124);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "sales@figkey.com";
             // 
             // Helper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(314, 134);
+            this.ClientSize = new System.Drawing.Size(350, 223);
+            this.Controls.Add(this.radLabel7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radLabel6);
+            this.Controls.Add(this.radLabel5);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radLabel4);
+            this.Controls.Add(this.radLabel3);
+            this.Controls.Add(this.radLabel2);
             this.Controls.Add(this.radButton1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.linkLabel1);
             this.Name = "Helper";
             // 
@@ -112,9 +186,17 @@
             // 
             this.RootElement.ApplyShapeToControl = true;
             this.Text = "帮助";
+            this.ThemeName = "Material";
             this.Load += new System.EventHandler(this.Helper_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel7)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,12 +204,18 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.Themes.MaterialTheme materialTheme1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+        private Telerik.WinControls.UI.RadLabel radLabel3;
+        private Telerik.WinControls.UI.RadLabel radLabel4;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel5;
+        private Telerik.WinControls.UI.RadLabel radLabel6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Telerik.WinControls.UI.RadLabel radLabel7;
+        private System.Windows.Forms.Label label1;
     }
 }
