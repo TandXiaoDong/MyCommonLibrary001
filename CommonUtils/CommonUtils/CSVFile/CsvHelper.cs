e17a8c59ce23a619db6ca55d10047205a1b1686d
1297953037@qq.com <1297953037@qq.com>
2019-12-25 18:16:15 +0800

    update

--- CommonUtils/CommonUtils/CSVFile/CsvHelper.cs
+++ CommonUtils/CommonUtils/CSVFile/CsvHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System.Data;
 using System.IO;
+using System.Windows.Forms;
+using System;
+using System.Text;
 
 namespace CommonUtils.SCVFile
 {
@@ -31,6 +34,53 @@ namespace CommonUtils.SCVFile
                         if (j > 0)
                             strBufferLine += ",";
                         strBufferLine += dt.Rows[i][j].ToString();
+                    }
+                    strmWriterObj.WriteLine(strBufferLine);
+                }
+                strmWriterObj.Close();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool SaveListViewSCV(ListView lv, string strFilePath, string tableheader, string columName)
+        {
+            try
+            {
+                if (lv.Items.Count < 1)
+                {
+                    MessageBox.Show("没有可以导出的数据！", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return false ;
+                }
+                SaveFileDialog saveDialog = new SaveFileDialog();
+                saveDialog.DefaultExt = "xls";
+                saveDialog.Filter = "xls文件 | *.xls";
+                saveDialog.FileName = strFilePath;
+                saveDialog.ShowDialog();
+                var strFileName = saveDialog.FileName;
+                if (string.IsNullOrEmpty(strFileName))
+                    return false;
+                if (File.Exists(strFileName))
+                    File.Delete(strFileName);
+                string strBufferLine = "";
+                if (String.IsNullOrEmpty(strFilePath))
+                    return false;
+                StreamWriter strmWriterObj = new StreamWriter(strFileName, false, Encoding.UTF8);
+                strmWriterObj.WriteLine(tableheader);
+                strmWriterObj.WriteLine(columName);
+                for (int i = 0; i < lv.Items.Count; i++)
+                {
+                    strBufferLine = "";
+                    for (int j = 0; j < lv.Columns.Count; j++)
+                    {
+                        //if(j == 0)
+                        //    strBufferLine.Append(lv.Items[i].Text);
+                        if (j > 0)
+                            strBufferLine += ",";
+                        strBufferLine += lv.Items[i].SubItems[j].Text;
                     }
                     strmWriterObj.WriteLine(strBufferLine);
                 }
---
+++ CommonUtils/WindowsFormTelerik/ControlCommon/GridDoubleBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using Telerik.WinControls.UI;
+
+namespace WindowsFormTelerik.ControlCommon
+{
+    public static class  GridDoubleBuffer
+    {
+        /// <summary>
+        /// 将给定的DataGridView设置双缓冲
+        /// </summary>
+        /// <param name="dgv">给定的DataGridView</param>
+        /// <param name="b">设置为ture即打开双缓冲</param>
+        public static void SetDoubleBuffered(this RadGridView dgv, bool b)
+        {
+            var dgvType = dgv.GetType();
+            var pi = dgvType.GetProperty("DoubleBuffered", BindingFlags.Instance | BindingFlags.NonPublic);
+            pi.SetValue(dgv, b, null);
+        }
+    }
+}
